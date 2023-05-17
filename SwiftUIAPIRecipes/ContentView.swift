18cd5c099f5b14297229d93be3dcfbfbe3700b6f
jonsalkin <50082962+jonsalkin@users.noreply.github.com>
2023-05-16 23:16:13 -0400

    Initial commit.

--- SwiftUIAPIRecipes/ContentView.swift
+++ SwiftUIAPIRecipes/ContentView.swift
@@ -5,22 +5,93 @@
 //  Created by Jon Salkin on 5/16/23.
 //
 
+
 import SwiftUI
 
-struct ContentView: View {
+struct URLImage: View {
+    let urlString: String
+    
+    @State var data: Data?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        if let data = data, let uiimage = UIImage(data: data) {
+            Image(uiImage: uiimage)
+                .resizable()
+                .aspectRatio(contentMode: .fill)
+                .frame(width: 130, height: 70)
+                .background(Color.gray)
         }
-        .padding()
+        else {
+            Image(systemName: "video")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 130, height: 70)
+                .background(Color.gray)
+                .onAppear {
+                    if let url = URL(string: urlString) {
+                        fetchData(from: url)
+                    }
+                }
+        }
+    }
+    
+    private func fetchData(from url: URL) {
+        
+        let task = URLSession.shared.dataTask(with: url) { data, _, _ in
+            self.data = data
+        }
+        task.resume()
+    }
+    
+}
+
+struct ContentView: View {
+    @StateObject var viewModel = ViewModel()
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(viewModel.recipes, id: \.idMeal) { recipe in
+                    HStack {
+                        URLImage(urlString: recipe.strMealThumb)
+                        
+                        Text(recipe.strMeal)
+                            .bold()
+                    }
+                    .padding(3)
+                }
+            }
+            .navigationTitle("Recipes")
+            .onAppear {
+                viewModel.fetch()
+            }
+        }
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+
+
+
+
+//MARK: - Test Images
+struct URLImageTestView: View {
+    let testImageURL = "https://www.themealdb.com//images//media//meals//adxcbq1619787919.jpg" // Replace with an actual test image URL
+
+    var body: some View {
+        URLImage(urlString: testImageURL)
+            .frame(width: 200, height: 200)
+    }
+}
+
+struct URLImageTestView_Previews: PreviewProvider {
+    static var previews: some View {
+        URLImageTestView()
     }
 }
---
+++ SwiftUIAPIRecipes/ViewModel.swift
@@ -0,0 +1,57 @@
+//
+//  ViewModel.swift
+//  SwiftUIAPIRecipes
+//
+//  Created by Jon Salkin on 5/16/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct Recipe: Hashable, Codable {
+    let idMeal: String
+    let strMeal: String
+    let strMealThumb: String
+}
+
+class ViewModel: ObservableObject {
+    @Published var recipes: [Recipe] = []
+    
+    func fetch() {
+        guard let url = URL(string: "https://themealdb.com/api/json/v1/1/filter.php?c=Dessert") else {
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) {[weak self] data, _, error in
+            guard let data = data, error == nil else {
+                return
+            }
+            
+            //Convert to JSON
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+                if let meals = json?["meals"] as? [[String: Any]] {
+                    let recipes = meals.compactMap { (mealDict: [String: Any]) -> Recipe? in
+                        guard let idMeal = mealDict["idMeal"] as? String,
+                              let strMeal = mealDict["strMeal"] as? String,
+                              let strMealThumb = mealDict["strMealThumb"] as? String else {
+                            return nil
+                        }
+                        
+                        return Recipe(idMeal: idMeal, strMeal: strMeal, strMealThumb: strMealThumb)
+                    }
+                    
+                    DispatchQueue.main.async {
+                        self?.recipes = recipes
+                    }
+                }
+            } catch {
+                print(error)
+            }
+        }
+        
+        task.resume()
+    }
+}
+
+
