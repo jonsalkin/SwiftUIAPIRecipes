770536737703b6d2e8654d505ed7bc34ab9f5185
jonsalkin <50082962+jonsalkin@users.noreply.github.com>
2023-05-16 22:13:51 -0400

    Initial Commit

---
+++ SwiftUIAPIRecipes/SwiftUIAPIRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAPIRecipesApp.swift
+//  SwiftUIAPIRecipes
+//
+//  Created by Jon Salkin on 5/16/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAPIRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
